b016ee280968af64e36c7ee3e888522fa31d2ff5
junesyi <junetheyi@gmail.com>
2024-10-15 22:26:31 -0700

    used clap crate to add command line instance for parsing and matching arguments

--- src/bin/kvdb.rs
+++ src/bin/kvdb.rs
@@ -1,25 +1,48 @@
 use kvdb::kvstore::kvstore::KvStore;
-use clap::Parser;
+use clap::{Parser, Command, Arg, Subcommand};
 
 #[derive(Parser, Debug)]
 struct Args {
     arg_cmd: String,
     arg_key: String,
-    arg_value: String,
+    arg_value: Option<String>,
 }
 
 fn main() {
-    println!("inside src/bin/kvdb.rs");
-    let args = Args::parse();
-    let first = args.arg_key;
-    let second = args.arg_value;
-    println!("first: {:?}, second: {:?}", first, second);
-
-    let mut store = KvStore::new();
-    // let a = KvStore::new("one".to_string(),"clown".to_string());
-    // let val = a.get_key();
-    // println!("{:?}", val);
-    // println!("{:?}", a);
+    let c = Command::new("kvdb")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .subcommand(
+            Command::new("set")
+            .about("Set a string value of a string key")
+                .arg(
+                    Arg::new("key")
+                        .help("The string key to set")
+                        .required(true))
+                .arg(
+                    Arg::new("value")
+                        .help("The string value to set")
+                        .required(true)
+                )
+        )
+        .subcommand(
+            Command::new("get")
+                .about("Get a string value of a string key")
+                .arg(
+                    Arg::new("key")
+                    .help("The key to get")
+                    .required(true)
+                )
+        )
+        .subcommand(
+            Command::new("rm")
+            .about("Remove a string value of a string key")
+                .arg(
+                    Arg::new("key")
+                        .help("The key to remove")
+                        .required(true)
+                )
+        )
+        .get_matches();
 }
-
-
