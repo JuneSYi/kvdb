4a27a592a698c4bc6449c991e50364e592c77382
junesyi <junetheyi@gmail.com>
2024-10-15 18:12:08 -0700

    fixed kvstore, started on kvdb binary

--- src/bin/kvdb.rs
+++ src/bin/kvdb.rs
@@ -1,8 +1,21 @@
 use kvdb::kvstore::kvstore::KvStore;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    arg_cmd: String,
+    arg_key: String,
+    arg_value: String,
+}
 
 fn main() {
     println!("inside src/bin/kvdb.rs");
-    panic!()
+    let args = Args::parse();
+    let first = args.arg_key;
+    let second = args.arg_value;
+    println!("first: {:?}, second: {:?}", first, second);
+
+    let mut store = KvStore::new();
     // let a = KvStore::new("one".to_string(),"clown".to_string());
     // let val = a.get_key();
     // println!("{:?}", val);
--- src/kvstore/kvstore.rs
+++ src/kvstore/kvstore.rs
@@ -1,28 +1,28 @@
+use std::collections::HashMap;
+
 #[derive(Debug)]
 pub struct KvStore {
-    key: String,
-    value: String,
+    map: HashMap<String, String>
 }
 
 impl KvStore {
     pub fn new() -> KvStore {
-        KvStore { key: "".to_string(), value: "".to_string() }
+        KvStore { map: HashMap::new() }
     }
 
     pub fn set(&mut self, key: String, value: String) {
-        self.key = key;
-        self.value = value;
+        self.map.insert(key, value);
     }
 
     pub fn get(&self, key: String) -> Option<String> {
-        match key == self.key {
-            true => Some(self.value.clone()),
-            false => None
+        match self.map.get(&key) {
+            Some(res) => Some(res.to_string()),
+            None => None
         }
     }
 
     pub fn remove(&mut self, key: String) {
-        // self.value.clone()
+        self.map.remove(&key);
     }
 
 
